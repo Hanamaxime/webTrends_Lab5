0e0b8d8b8f55cf1593985905ed20de6cc946f923
Hana <hanamaxime@gmail.com>
2018-06-10 09:33:29 -0400

    first commit

---
+++ src/app/person.ts
@@ -0,0 +1,27 @@
+//using a number a date and a meathod
+
+export class Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+
+//   getAge(): number {
+//
+//     let today: Date = new Date();
+//
+//     let age: number =today.getFullYear() - this.dateOfBirth.getFullYear();
+//
+//     if( this.dateOfBirth.getDate() > today.getDate()
+//     &&
+//     this.dateOfBirth.getMonth() > today.getMonth()
+//     ) {
+//
+//       age--; //increase the age by one
+//
+//     }
+//
+//     return age;
+//
+//   }
+ }
